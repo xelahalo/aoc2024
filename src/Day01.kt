aa77f8dfb6a3707a15e723e2256ec19456184a12
Oláh Alex <Olah.Alex@stud.u-szeged.hu>
2024-12-01 11:17:06 +0100

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,32 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { line ->
+            val (a, b) = line.split("\\s+".toRegex()).map { it.toInt() }
+            a to b
+        }.unzip()
+
+        return left.sorted().zip(right.sorted()).sumOf { (a, b) -> abs(b - a)  }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableMapOf<Int, Int>()
+
+        input.forEach { line ->
+            val (a,b) = line.split("\\s+".toRegex()).map { it.toInt() }
+            left.add(a)
+            right[b] = right.getOrDefault(b, 0) + 1
+        }
+
+        return left.sumOf { it * (right[it] ?: 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
